4a2e1f0b1790fbbe031406634d34564ea2cb11ee
EndreusSre <endreus.valerio@gmail.com>
2025-10-02 13:01:15 -0300

    organizando arquivos

---
+++ scripts/script.js
@@ -0,0 +1,60 @@
+//botões
+const pikachu = document.querySelector("#pikachu")
+const charmander = document.querySelector("#charmander")
+const bulbasaur = document.querySelector("#bulbasaur")
+const snorlax = document.querySelector("#snorlax")
+
+//variaveis
+let main = document.querySelector("#main")
+
+pikachu.addEventListener("click", function () {
+    main.innerHTML = ""
+    fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+        .then(res => res.json())
+        .then(data => {
+            main.innerHTML += `<img src="${data.sprites.front_default}">`
+            main.innerHTML += `<p>Nome: ${data.name}</p>`
+            main.innerHTML += `<p>Altura: ${data.height}m</p>`
+            main.innerHTML += `<p>Peso: ${data.weight}kg</p>`
+        })
+        .catch(err => console.error("ERRO:", err))
+})
+
+charmander.addEventListener("click", function () {
+    main.innerHTML = ""
+    fetch("https://pokeapi.co/api/v2/pokemon/charmander")
+        .then(res => res.json())
+        .then(data => {
+            main.innerHTML += `<img src="${data.sprites.front_default}">`
+            main.innerHTML += `<p>Nome: ${data.name}</p>`
+            main.innerHTML += `<p>Altura: ${data.height}m</p>`
+            main.innerHTML += `<p>Peso: ${data.weight}kg</p>`
+        })
+        .catch(err => console.log("ERRO: ", err))
+})
+
+bulbasaur.addEventListener("click", function () {
+    main.innerHTML = ""
+    fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur")
+        .then(res => res.json())
+        .then(data => {
+            main.innerHTML += `<img src="${data.sprites.front_default}">`
+            main.innerHTML += `<p>Nome: ${data.name}</p>`
+            main.innerHTML += `<p>Altura: ${data.height}m</p>`
+            main.innerHTML += `<p>Peso: ${data.weight}kg</p>`
+        })
+        .catch(err => console.log("ERRO: ", err))
+})
+
+snorlax.addEventListener("click", function () {
+    main.innerHTML = ""
+    fetch("https://pokeapi.co/api/v2/pokemon/snorlax")
+        .then(res => res.json())
+        .then(data => {
+            main.innerHTML += `<img src="${data.sprites.front_default}">`
+            main.innerHTML += `<p>Nome: ${data.name}</p>`
+            main.innerHTML += `<p>Altura: ${data.height}m</p>`
+            main.innerHTML += `<p>Peso: ${data.weight}kg</p>`
+        })
+        .catch(err => console.log("ERRO: ", err))
+})
